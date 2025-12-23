c27ca991ea454a4766adc8a0d075e0f4242aeff8
Tushar Singh Rawat <117996478+tusharsinghrawat@users.noreply.github.com>
2025-12-23 08:41:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base : "./My-Portfolio-Website-Project/",
+  base : "/Portfolio-Website-Project/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
